cee78da0f85059f970866c5255af118eff5aacec
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-03-12 21:43:36 +0530

    made some final changes

--- script.js
+++ script.js
@@ -45,6 +45,8 @@ const generateBotResponse = async(incomingMessageDiv) => {
     }
     catch(error){
         console.log(error);
+        messageElement.innerText = error.message;
+        messageElement.style.color = "#ff0000";
     }
     finally{
         incomingMessageDiv.classList.remove("thinking");
